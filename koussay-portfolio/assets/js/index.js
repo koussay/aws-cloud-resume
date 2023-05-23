c116ae6097005f07d57aaa52b140384768f89ab5
koussay <boualikoussay7@gmail.com>
2023-05-23 12:41:56 +0100

    initial commit

---
+++ koussay-portfolio/assets/js/index.js
@@ -0,0 +1,13 @@
+
+/*
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch(
+        "https://q5qshm3pj2.execute-api.eu-north-1.amazonaws.com/dev/visitors/"
+    );
+    let data = await response.json();
+    counter.innerHTML = ` 👀 Views: ${data}`;
+}
+updateCounter();
+
+ */
